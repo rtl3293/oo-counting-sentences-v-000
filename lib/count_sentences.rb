37325883614b950857967a11da2074cb9302c951
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-29 02:44:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    sentences = self.split(/\w+/)
+  
+    sentences.delete("")
+    sentences.delete(" ")
+    sentences.delete(", ")
+    #sentences = ["This, well, is a sentence.", "This is too!! And so is this, I think? Woo.""".""."]
+    sentences.count
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
