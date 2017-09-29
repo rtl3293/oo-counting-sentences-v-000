ea8e09f66e2f8754f421fb80cf7541a8f70167fb
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-29 02:34:24 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,7 +17,7 @@ class String
   def count_sentences
     sentences = self.split(".")
     sentences.delete(".")
-    #sentences = ["This, well, is a sentence.", "This is too!! And so is this, I think? Woo.""".""."]
+    #sentences = ["This, well, is a sentence.", "This is too!! And so is this, I think? Woo."]
     sentences.each do |sentence|
 
     sentences.count
